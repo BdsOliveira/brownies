245208ca8e191ffffc378dd63f11b2089226f94e
BdsOliveira <bds.commus@gmail.com>
2022-10-11 15:20:14 -0300

    Create the OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Seller;
+use App\Models\Company;
 
 class OrderController extends Controller
 {
     public function orders(){
-        return view('/orders/create-order');
+        $companies = Company::all();
+        $sellers = Seller::all();
+        return view('/orders/create-order', ['companies' => $companies, 'sellers' => $sellers]);
     }
 
 }
