07da7aed319c5a30f20182591ac0771bb48963f8
Bruno Oliveira <bds.commus@gmail.com>
2023-01-13 20:20:24 -0300

    feat: add SellerController crud functions

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Seller;
-use App\Models\Company;
 
 class SellerController extends Controller
 {
     public function index()
     {
-        return response()->json(Seller::get());
+        return Seller::paginate(10);
+    }
+
+    public function show($id)
+    {
+        return Seller::get()->find($id);
     }
 
     public function store(Request $request)
@@ -20,65 +24,11 @@ class SellerController extends Controller
 
     public function update(Request $request)
     {
-        Seller::findOrFail($request->id)->update($request->all());
-        return response()->json(
-            [
-                'success'
-            ],
-            200,
-        );
+        return Seller::findOrFail($request->id)->update($request->all());
     }
 
     public function delete($id)
     {
-        Seller::findOrFail($id)->delete();
-        return response()->json(
-            [
-                'success'
-            ],
-            200,
-        );
+        return Seller::findOrFail($id)->delete();
     }
-
-    // Old endpoints
-    // public function viewSellers()
-    // {
-    //     $sellers = Seller::all();
-    //     return view('sellers/sellers', ['sellers' => $sellers]);
-    // }
-
-    // public function viewCreateSeller()
-    // {
-    //     $companies = Company::all();
-    //     return view('/sellers/create-seller', ['companies' => $companies]);
-    // }
-
-    // public function store(Request $request)
-    // {
-    //     $seller = new Seller;
-
-    //     $seller->fill($request->all());
-    //     $seller->save();
-
-    //     return redirect('/management/sellers')->with('msg', 'Vendedor cadastrado com sucesso!');;
-    // }
-
-    // public function destroy($id)
-    // {
-    //     Seller::findOrFail($id)->delete();
-    //     return redirect('/management/sellers')->with('msg', 'Vendedor excluído com sucesso.');
-    // }
-
-    // public function edit($id)
-    // {
-    //     $seller = Seller::findOrFail($id);
-    //     $companies = Company::all();
-    //     return view('/sellers/edit-seller', ['seller' => $seller, 'companies' => $companies]);
-    // }
-
-    // public function update(Request $req)
-    // {
-    //     Seller::findOrFail($req->id)->update($req->all());
-    //     return redirect('/management/sellers')->with('msg', 'Vendedor editado com sucesso.');
-    // }
 }
