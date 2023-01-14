5c4cb9969d54fe512e9e3870abe88626d34bb4e3
Bruno Oliveira <bds.commus@gmail.com>
2023-01-13 23:02:59 -0300

    feat: add columns and relationship to orders table

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,7 +11,7 @@ class OrderController extends Controller
 {
     public function index()
     {
-        return response()->json(Order::get());
+        return Order::with('seller')->paginate(15);
     }
 
     public function store(Request $request)
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,16 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     use HasFactory;
-    protected $fillable = ['id_seller', 'quantitySold', 'dateSell'];
+    use HasUuids;
 
     public function seller()
     {
-        return $this->belongsTo(Seller::class, 'id_seller' );
+        return $this->belongsTo(Seller::class);
     }
+
+    protected $fillable = ['id_seller', 'quantitySold', 'dateSell'];
 }
