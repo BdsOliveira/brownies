7bf033601fb11713f178687b4750a8c86577ff96
Bruno Oliveira <bds.commus@gmail.com>
2023-06-12 22:41:29 -0300

    create migrations from database

---
+++ database/migrations/2023_06_13_005517_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->char('id', 36)->primary();
+            $table->unsignedBigInteger('company_id');
+            $table->text('name');
+            $table->double('price', 8, 2);
+            $table->string('description', 144);
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2023_06_13_005517_create_sellers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sellers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_company')->nullable()->index('id_company');
+            $table->string('nameSeller', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sellers');
+    }
+};
